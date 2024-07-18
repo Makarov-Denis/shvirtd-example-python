e9d55a98253ff5ec3fcd65a836f538709e36f83a
Makarov-Denis <148921246+Makarov-Denis@users.noreply.github.com>
2024-07-19 02:55:23 +0300

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -4,7 +4,7 @@ cd Netology
 git init
 git config user.name vladrabbit
 git config user.email vlad.rabbit@gmail.com
-git remote add origin git@github.com:vladrabbit/shvirtd-example-python.git
+git remote add origin git@github.com:Makarov-Denis/shvirtd-example-python.git
 git checkout -b main
 git pull origin main
 docker compose up -d
